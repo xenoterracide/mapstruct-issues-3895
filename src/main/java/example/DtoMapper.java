4c68f7bedb3b4f7f1e43fa990f8070c75236e81d
Caleb Cushing <caleb.cushing@gmail.com>
2025-06-27 13:36:06 -0400

    expression actually works here

--- src/main/java/example/DtoMapper.java
+++ src/main/java/example/DtoMapper.java
@@ -2,6 +2,7 @@ package example;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(uses = { ConverterMapper.class, Headers.class })
 public interface DtoMapper {
@@ -10,4 +11,7 @@ public interface DtoMapper {
   MyDto toDto(MyEntity entity);
 
   MyError fromException(MyException exception);
+
+  @Mapping(target = "name", source = "name")
+  void copyTo(MyDto source, @MappingTarget ImmutableMyEntity.Builder target);
 }
